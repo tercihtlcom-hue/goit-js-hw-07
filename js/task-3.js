412b49ed62c78d41914fc4ebccc34e99897801e4
Arif Yazici <tercihtl.com@gmail.com>
2026-03-30 01:34:22 +0300

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", () => {
+  const value = input.value.trim();
+  output.textContent = value === "" ? "Anonymous" : value;
+});
---
+++ js/task-6.js
@@ -0,0 +1,43 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.margin = "5px";
+    elements.push(div);
+    size += 10;
+  }
+
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
