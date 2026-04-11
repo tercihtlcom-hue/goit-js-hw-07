dec0968f84b07121a8a9760962080172234eab83
Arif Yazici <tercihtl.com@gmail.com>
2026-04-11 14:36:20 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
+{
+  const input = document.querySelector("#name-input");
+  const output = document.querySelector("#name-output");
 
-input.addEventListener("input", () => {
-  const value = input.value.trim();
-  output.textContent = value === "" ? "Anonymous" : value;
-});
+  input.addEventListener("input", () => {
+    const value = input.value.trim();
+    output.textContent = value === "" ? "Anonymous" : value;
+  });
+}
